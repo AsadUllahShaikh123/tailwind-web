98ad81f24ae4fb1f8d24adcec06d4ac8d6953abf
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2022-06-20 07:21:47 -0700

    done with image uploading

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { useState } from "react";
 import Suggestion from "./Suggestion";
 
 let suggestion =
   "Recommended cover photo size is 960 x 640 pixels, with an aspect ratio of 3:2;";
 
-let handleClick =()=>{
-  document.getElementById('file').click();
-}
+
 const InfoSection = () => {
+  let [image,setImage] = useState({
+    file1:'',
+    file2:''
+  });
+  let handleClick = (e) => {
+    document.getElementById(e.target.name).click();
+    
+  };
+  let handleChange =(e)=>{
+    let {name} = e.target;
+    let preview = URL.createObjectURL(e.target.files[0])
+    setImage({...image , [name]:preview});
+  }
   return (
     <>
       <h1 style={{ color: "rgb(243, 244, 246)", fontSize: "1.5rem" }}>
@@ -18,10 +30,12 @@ const InfoSection = () => {
         className="stepC"
         style={{ display: "flex", flexWrap: "wrap", marginTop: "1.5rem" }}
       >
+        <img src={image.file1} alt="" srcset="" style={{width:'100px',height:'100px'}}/>
         <div className="button">
-          <input type="file" id="file" style={{ display: "none" }} />
+          <input type="file" name="file1" id="file1" accept="image/*" onChange={handleChange} style={{ display: "none" }} />
           <button
-           onClick={handleClick}
+           name="file1"
+            onClick={handleClick}
             style={{
               color: "white",
               fontSize: "1.5rem",
@@ -48,19 +62,25 @@ const InfoSection = () => {
         className="stepC"
         style={{ display: "flex", flexWrap: "wrap", marginTop: "1.5rem" }}
       >
-        <button
-          style={{
-            color: "white",
-            fontSize: "1.5rem",
-            marginRight: "1rem",
-            padding: "0.4rem 1rem",
-            backgroundColor: "rgb(75 85 99)",
-            borderRadius: "4px",
-            fontWeight: "800",
-          }}
-        >
-          +
-        </button>
+        <img src={image.file2} alt="" srcset="" style={{width:'100px',height:'100px'}} />
+        <div className="button">
+          <input type="file" name="file2" id="file2" accept="image/*" onChange={handleChange} style={{display:'none'}}/>
+          <button
+            name="file2"
+            onClick={handleClick}
+            style={{
+              color: "white",
+              fontSize: "1.5rem",
+              marginRight: "1rem",
+              padding: "0.4rem 1rem",
+              backgroundColor: "rgb(75 85 99)",
+              borderRadius: "4px",
+              fontWeight: "800",
+            }}
+          >
+            +
+          </button>
+        </div>
         <div className="text">
           <p style={{ color: "rgb(243, 244, 246)", fontWeight: "400" }}>
             Add Logo
